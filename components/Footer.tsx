c58d508c09590e2650e8da3a7b81db03c91c281d
Xspencer6 <spenceralmodiel@gmail.com>
2026-02-10 17:33:07 +0800

    MODIFIED FOOTER.TSX: Removed redundant information, added company information with clickable facebook link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -81,7 +81,7 @@ export default function Footer() {
         {/* Bottom Bar */}
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-sm">
-            &copy; 2026 NJOY Travel & Tours. All rights reserved.
+            &copy; 2022 NJOY Travel & Tours. All rights reserved.
           </p>
         </div>
       </div>
